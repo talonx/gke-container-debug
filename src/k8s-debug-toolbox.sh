da0bd31dc4ffa947ed969098cf4cb063257283b3
Hrishikesh Barua <hrishikesh@inceptioncloud.io>
2020-09-21 12:42:35 +0530

    Removed unneeded key dependency and relative path for the dump.

--- src/k8s-debug-toolbox.sh
+++ src/k8s-debug-toolbox.sh
@@ -10,9 +10,6 @@ filename=$1
 bucket=$2
 user=$3
 
-dir=`df | awk '{print $6}' | grep "volumes/kubernetes.io~secret/google-cloud-key" | head -n 1`
-echo "Key vol ${dir}"
-/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=${dir}/key.json
 echo "Uploading to bucket"
 /google-cloud-sdk/bin/gsutil cp /media/root/home/${user}/${filename} gs://${bucket}/kdev-debug/${filename}
 echo "Final location ${bucket}/kdev-debug/$filename"
--- src/k8s-debug-vm.sh
+++ src/k8s-debug-vm.sh
@@ -15,11 +15,11 @@ echo "Taking ${action} on ${pod_name}"
 container_id=`docker ps | grep ${pod_name} | grep -v "POD" | awk '{print $1}'`
 echo "Container id ${container_id}"
 #Supports only heap dump for now
-docker exec ${container_id} sh -c "jmap -dump:format=b,file=heap.dump 1"
+docker exec ${container_id} sh -c "jmap -dump:format=b,file=/heap.dump 1"
 dttime=`echo $(date '+%d-%b-%Y-%H-%M-%S')`
 filename=${pod_name}-${dttime}.hdump
 echo "Copying heap dump to node"
-docker cp ${container_id}:heap.dump ${filename}
+docker cp ${container_id}:/heap.dump ${filename}
 echo "Heap dump at ${filename}"
 /usr/bin/toolbox bash /media/root/home/${USER}/k8s-debug-toolbox.sh ${filename} ${bucket} ${USER}
 
